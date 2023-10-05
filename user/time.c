effa971e587b7a720e64737295b42ef89ba94bff
dloyavilla <dloyavilla@miners.utep.edu>
2023-10-04 22:03:09 -0600

    homework 2

--- user/time.c
+++ user/time.c
@@ -10,27 +10,31 @@ main(int argc, char **argv)
 {
   int i, start, elapsed;
   struct rusage ru;
-  
+  // check if we have two arguments
   if (argc < 2){
   	printf("Usage: time <comm> [args...]");
         exit(-1);
    }
-
+ // shifting left and 0
   for(i=1; i<sizeof(argv); i++) {
   	newargv[i-1] = argv[i];
   }
   newargv[sizeof(argv)-1] = 0;
+  // uptime and child 
   start = uptime();
   printf("Time: %d\n", start);
   int pid = fork();
+  
   if(pid == -1) {
   	printf("fork failed\n");
   	exit(1);
   } else if (pid == 0) {
+  	// call exec
   	exec(newargv[0], newargv);
   	printf("exec failed\n");
   	exit(1);
   } else {
+  	// get the cputime 
   	int wpid = wait2(0, &ru);
   	elapsed = uptime() - start;
         printf("elapsed time: %d ticks, cpu time: %d ticks, %d% CPU\n", elapsed, ru.cputime, ru.cputime*100/elapsed);
